6e634aac30a7797e0d01afb39aee4ffe50a89964
Lutetium-Vanadium <luv.s7000@gmail.com>
2020-02-26 20:50:22 +0530

    Initial Commit

---
+++ Frontend/main.mjs
@@ -0,0 +1,24 @@
+import { randint } from "./functions/random.mjs";
+
+let type = "WebGL";
+if (!PIXI.utils.isWebGLSupported) {
+  type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+
+let app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  antialias: true,
+  autoResize: true
+  // transparent: true
+});
+document.body.appendChild(app.view);
+
+app.renderer.view.style.postion = "absolute";
+app.renderer.view.style.display = "block";
+
+document.getElementById("bg").src = `./backgrounds/${randint(3)}.jpg`;
+
+// const loader = new PIXI.Loader();
